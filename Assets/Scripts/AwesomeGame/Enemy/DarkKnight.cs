472d7b4f89b3083770ccb125dbe2af056a8683e1
donmetapod <tyrantmx@gmail.com>
2023-07-01 12:12:00 -0600

    Created folders, moved scripts to correspondent folders

--- Assets/Scripts/AwesomeGame/Enemy/DarkKnight.cs
+++ Assets/Scripts/AwesomeGame/Enemy/DarkKnight.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class DarkKnight : MonoBehaviour
 {
     CharacterConfiguration _characterConfiguration;
-        
+            
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
